1ec283d3ed667efef51bf0546ea2b108a2267ca0
Simon <dazhouqing@163.com>
2019-04-29 19:23:36 +0800

    init filed

---
+++ Map/Map.go
@@ -0,0 +1,12 @@
+package Map
+
+/**
+ * @version: go1.12.4 darwin/amd64
+ * @Author : Simon
+ * @Explain :
+ * @contact:
+ * @Time : 2019-04-29  19:19
+ * @File : Map
+ * @Software: GoLand 2018.3
+ */
+
