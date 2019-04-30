cc1bc740aaaaedabe6d08ef0203ac59a2b874cb1
Simon <dazhouqing@163.com>
2019-04-30 10:33:15 +0800

    add linear list explain

--- Linear-List/LinearList.go
+++ Linear-List/LinearList.go
@@ -3,7 +3,7 @@ package Linear_List
 /**
  * @version: go1.12.4 darwin/amd64
  * @Author : Simon
- * @Explain :
+ * @Explain : 线性表---零个或多个数据元素的有限序列
  * @contact:
  * @Time : 2019-04-29  19:19
  * @File : Map
